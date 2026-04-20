ab4c8a69cef94eceef32b2d65e5059dfdd363329
Yevhenii <ewgeshka06@gmail.com>
2026-04-20 17:50:25 +0300

    Fix formatting and comments in KeyProcessor.cpp

--- lab6/src/KeyProcessor.cpp
+++ lab6/src/KeyProcessor.cpp
@@ -5,5 +5,5 @@ Mode KeyProcessor::getMode(int key, Mode currentMode) {
     if (key == '2') return Mode::GRAY;
     if (key == '3') return Mode::CANNY;
     if (key == '4') return Mode::BLUR;
-    return currentMode; // Якщо інша клавіша — лишає поточний режим
+    return currentMode; // якщо інша клавіша — лишає поточний режим
 }
