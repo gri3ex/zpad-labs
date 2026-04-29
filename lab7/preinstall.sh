4b6f9aefca52cd45563b7deb0a9f11a4daabe024
Yevhenii <ewgeshka06@gmail.com>
2026-04-29 14:10:57 +0300

    Залишив тільки 2-6

--- lab7/preinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt install libopencv-dev cmake g++ wget -y
-wget -nc https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
-wget -nc https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel
--- lab7/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./build/lab7_exe
