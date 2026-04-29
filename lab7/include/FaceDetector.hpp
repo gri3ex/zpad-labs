acb77faad14536b2be50e89ba8f30a6f40459071
Yevhenii <ewgeshka06@gmail.com>
2026-04-29 14:22:07 +0300

    Add files via upload

--- lab7/include/FaceDetector.hpp
+++ lab7/include/FaceDetector.hpp
@@ -1,1 +1,28 @@
-ff
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <vector>
+
+class FaceDetector {
+public:
+    FaceDetector();
+    ~FaceDetector();
+    
+    void setFrame(const cv::Mat& frame);
+    std::vector<cv::Rect> getFaces();
+
+private:
+    void worker(); // функція для фонового потоку
+
+    cv::dnn::Net net;
+    std::thread workerThread;
+    std::mutex mtx;
+    std::atomic<bool> running;
+    
+    bool newFrameAvailable;
+    cv::Mat currentFrame;
+    std::vector<cv::Rect> detectedFaces;
+};
